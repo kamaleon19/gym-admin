1fdb063629f5643a9c9e2ff39dadf5c5526da6a1
kamaleon19 <joaco.px@gmail.com>
2025-10-14 16:31:28 -0300

    Readme updated

--- src/whatsapp/whatsapp.service.ts
+++ src/whatsapp/whatsapp.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { Partners } from '@prisma/client';
-import { PrismaService } from 'prisma/prisma.service';
+
 import { Twilio } from 'twilio';
 
 @Injectable()
